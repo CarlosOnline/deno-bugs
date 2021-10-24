03379b48564a11527fa7f27a656cb80190031768
CarlosOnline <cgomes@iinet.com>
2021-10-24 16:59:41 -0700

    Simplify write-output

--- write-output.ts
+++ write-output.ts
@@ -1,15 +1,4 @@
-if (!Deno.args.length) {
-  console.error("Missing arguments. Please pass number of characters to emit");
-  Deno.exit(-1);
-}
-
-const length = parseInt(Deno.args[0]);
-if (!length) {
-  console.error(
-    "Please pass valid number of characters to emit greater than 0"
-  );
-  Deno.exit(-1);
-}
-
+const arg = Deno.args[0];
+const length = parseInt(arg);
 const output = "a".repeat(length);
 console.log(output);
